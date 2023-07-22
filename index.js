38bb177ecbbcb1b104757b00b54c658c7dd77e88
tuannv0069 <tuannv0069@gmail.com>
2023-07-23 00:01:59 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000; // Change this to any port number you prefer
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
